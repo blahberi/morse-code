3372ed85a152a66e7eb8447ea62c3dcb3adcf390
blahberi <along.blabri@gmail.com>
2021-10-01 23:56:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import morse_code
+import re
+
+input_string = input()
+
+isMorse = False
+
+if input_string.split(' ')[0] in morse_code.morse_to_letters.keys():
+    isMorse = True
+
+res = ""
+if isMorse:
+    words = re.split('   | / ', input_string)
+    for word in words:
+        letters = word.split(' ')
+        for letter in letters:
+            res += morse_code.morse_to_letters[letter]
+        res += ' '
+else:
+    words = re.split(' ', input_string)
+    for word in words:
+        letters = list(word)
+        for letter in letters:
+            res += morse_code.letters_to_morse[letter] + ' '
+        res += ' / '
+
+print(res)
