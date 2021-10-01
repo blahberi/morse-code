2c662396c23fa4e20eced18dda9decbacffb8f5c
blahberi <along.blabri@gmail.com>
2021-10-02 00:04:09 +0300

    fixed some bugs

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ if input_string.split(' ')[0] in morse_code.morse_to_letters.keys():
 
 res = ""
 if isMorse:
-    words = re.split('   | / ', input_string)
+    words = re.split('   |  / ', input_string)
     for word in words:
         letters = word.split(' ')
         for letter in letters:
             res += morse_code.morse_to_letters[letter]
         res += ' '
+    res = res[:-1]
 else:
     words = re.split(' ', input_string)
     for word in words:
@@ -23,5 +24,6 @@ else:
         for letter in letters:
             res += morse_code.letters_to_morse[letter] + ' '
         res += ' / '
+    res = res[:-3]
 
 print(res)
